054d7bf08ecb7ed5009dd51c3de99bcf95f3d2b9
stephanecha <schamblain@gmail.com>
2018-09-26 10:31:23 +0200

    ajout d'un controleur Archer et d'une classe admin

---
+++ Archery/Models/Administrator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Archery.Models
+{
+    public sealed class Administrator : User
+    {
+
+    }
+}
---
+++ Archery/Models/Archer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Archery.Models
+{
+    public class Archer : User
+    {
+        public string LicenseNumber { get; set; }
+    }
+}
