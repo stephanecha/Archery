abeb62d5cb284aa9c8a24f9f08a1428e2ec81013
stephanecha <schamblain@gmail.com>
2018-10-01 17:01:14 +0200

    ajout des classes tournoi shooter

--- Archery/Data/ArcheryDbContext.cs
+++ Archery/Data/ArcheryDbContext.cs
@@ -9,13 +9,24 @@ namespace Archery.Data
 {
     public class ArcheryDbContext : DbContext
     {
+
+
+
+
         public ArcheryDbContext() : base("Archery")
         {
-
-
         }
+
         public DbSet<Administrator> Administrators { get; set; }
+
         public DbSet<Archer> Archers { get; set; }
 
+        public DbSet<Tournament> Tournaments { get; set; }
+
+        public DbSet<Weapon> Weapons { get; set; }
+
+        public DbSet<Shooter> Shooters { get; set; }
     }
+
+
 }
