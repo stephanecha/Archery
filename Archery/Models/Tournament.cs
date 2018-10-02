b094178eff66e4c4cf611306b52268bee2cd3691
stephanecha <schamblain@gmail.com>
2018-10-02 17:15:38 +0200

    Ajout des Weapons dans la creation et affichage d'un tournoi

--- Archery/Models/Tournament.cs
+++ Archery/Models/Tournament.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Archery.Models
 {
@@ -21,6 +22,7 @@ namespace Archery.Models
 
         [Display(Name = "Description")]
         [DataType(DataType.MultilineText)]
+        [AllowHtml]
         public string Description { get; set; }
 
         [Required]
