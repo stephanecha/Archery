a18bcb43757b327a237c5f2f8d24100ccaf7879c
stephanecha <schamblain@gmail.com>
2018-10-02 10:36:26 +0200

    ajout scalffold pour la partie weapon dans le backoffice

--- Archery/Models/Tournament.cs
+++ Archery/Models/Tournament.cs
@@ -14,6 +14,12 @@ namespace Archery.Models
         public string Name { get; set; }
 
         [Required]
+        [Display(Name = "Lieu")]
+        [StringLength(250)]
+        [DataType(DataType.MultilineText)]
+        public string Location { get; set; }
+
+        [Required]
         [Display(Name = "Début")]
         public DateTime StartDate { get; set; }
 
